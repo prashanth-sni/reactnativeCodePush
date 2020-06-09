ac127efe086eb16fec9234ff4b4a7d5b1d070285
prashanth-sni <prashanth.k@appliedsni.com>
2020-06-09 08:17:50 +0530

    codepush demo commit

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Well come to React Native </Text>
+      <Text style={styles.text}>Add any feature to app buy editing App.js </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    padding:20
+  },
+  text:{
+    fontSize:18
+  }
+});
+
+export default App;
