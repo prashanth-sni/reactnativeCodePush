1065c84d6eff575578fd472dca9b5126f3cbe7dc
prashanth-sni <prashanth.k@appliedsni.com>
2020-06-09 17:04:56 +0530

    code change for demo changes

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:"center",
     padding:20,
-    backgroundColor:'tomato'
+    backgroundColor:'white'
   },
   text:{
     fontSize:18
